29b58f792f129c03d8c8733698a557f479d2cba2
Aurelian <aurelian.stanica@yahoo.co.uk>
2017-10-10 15:23:51 +0100

    Add files via upload

---
+++ fingers.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+document.getElementById("button").addEventListener("click", Function() {
+                                                   
+         var but = document.getElementById("fin").style.color = "yellow";                                          
+});
