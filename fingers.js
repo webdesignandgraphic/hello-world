315436dcb6fb64d008642dffa53ad9037bbbd31c
Aurelian <aurelian.stanica@yahoo.co.uk>
2018-06-09 19:34:45 +0100

    Delete fingers.js

--- fingers.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-document.getElementById("button").addEventListener("click", Function() {
-                                                   
-         var but = document.getElementById("fin").style.color = "yellow";                                          
-});
